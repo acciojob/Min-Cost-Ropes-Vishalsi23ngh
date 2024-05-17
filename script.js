45764b9ec767b6dbf5562a685d76ced761ec29a2
Vishal <100362137+Vishalsi23ngh@users.noreply.github.com>
2024-05-17 19:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort();
+	int cost = 0
+	int rope = arr[0];
+	for (let index = 1; index < arr.length; i++){
+		sum = cost + arr[i];
+		cost = sum;
+	}
+  return sum;
 }
 
 module.exports=mincost;
